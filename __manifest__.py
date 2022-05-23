275bc907dbf50856ed20146449523659c2cf89cf
Nelson V <nelson.avelez@hotmail.com>
2022-05-23 14:42:31 -0500

    Menú Horas Extra, Etiq. Analitica

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'website': "https://www.intresco.co",
     'category': 'HR',
     'version': '14.0.0.1',
-    'depends': ['analytic'],
+    'depends': ['analytic','bi_hr_overtime_request_comm'],
     'data': [
         'views/views.xml',
     ],
